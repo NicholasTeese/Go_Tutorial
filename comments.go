93338d3b1cb92a20fa96259c5a11150b6c004d4a
Unknown <Mr.Teese@hotmail.com>
2020-07-27 15:55:48 +1000

    Tutorial
    Began Golang tutorial on TutorialsPoint website.
    covered between subjects Home and Functions.
    stopped early due to company arriving.
    Added Comments page for commentary and quotes.

---
+++ comments.go
@@ -0,0 +1,29 @@
+/*
+This page will contain self commentary to make it easier to remember things,
+as well as reminding me where I left off from my previous sessions.
+It will also have quotes and info I find helpful.
+
+"The Go compiler internally places “;” as the statement terminator to indicate the end of one logical entity."
+^ This is amazing.
+
+Most of what I am covering here is just reading, so there isn't much coding to do.package main
+
+derived types:
+struggled with pointers in uni - will look into it more
+union/slice/interface/map/channel types all sound new to me.
+Will delve more into them as they come up.
+
+byte == uint8
+rune == int32
+uintptr "an unsigned integer to store the uninterpreted bits of a pointer value"
+
+"lvalue − Expressions that refer to a memory location is called "lvalue" expression.
+An lvalue may appear as either the left-hand or right-hand side of an assignment."
+"rvalue − The term rvalue refers to a data value that is stored at some address in memory.
+An rvalue is an expression that cannot have a value assigned to it which means an rvalue may appear on the right-
+but not left-hand side of an assignment."
+Ivalue = data location
+Rvalue = data
+
+
+*/
--- main.go
+++ main.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
+//func main() {
+//http.HandleFunc("/", HelloServer)
+//http.ListenAndServe(":8080", nil)
+/*
+	fmt.Println("hello world")
+  	Everything above was a short re-introduction to coding.
+	Everything hence forth will be the official tutorial into Golang
+	some of which will be re-hashing previously covered things,
+	but for the sake of effective study I will continue with them all the same.	*/
+//tutorial can be found at https://www.tutorialspoint.com/go/go_program_structure.htm
 
-	//fmt.Println("hello world")
+func main() {
+	/*This is my first sample program.*/
+	fmt.Println("Hello, World!")
+
+	//variables tutorial
+	var x float64
+	x = 20.0
+	y := 42
+	c = "foo"
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(c)
+	fmt.Printf("x is of type %T\n", x)
+	fmt.Printf("y is of type %T\n", y)
+	fmt.Printf("c is of type %T\n", c)
 }
 
-// HelloServer reads from the file "index" and returns the data inside, in the event of an error it will print the error instead
-func HelloServer(w http.ResponseWriter, r *http.Request) {
+// HelloServer reads from the file "index" and returns the data inside
+// in the event of an error it will print the error instead
+/*func HelloServer(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	//w.Write([]byte("<h1>Hello Server</h1>"))
 	data, err := ioutil.ReadFile("index.html")
@@ -22,4 +43,4 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 	w.Write(data)
-}
+}*/
